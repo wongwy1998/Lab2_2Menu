40b1a718e6fb023d1077fd26d800b55d38d0468b
wei yao <wongwy-wa16@student.tarc.edu.my>
2018-11-01 16:57:08 +0800

    commit

---
+++ app/src/main/java/my/edu/tarc/lab2_2menu/AboutUsActivity.java
@@ -0,0 +1,62 @@
+package my.edu.tarc.lab2_2menu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.nio.channels.InterruptedByTimeoutException;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    public void visitTARUC(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.taruc.edu.my"));
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }else{
+            /*Log.d("Visit TARUC","No able to handle intent.");*/
+            Toast.makeText(this,"No app to handle this",Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    public void showMain(View view){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        startActivity(intent);
+
+    }
+
+    public void showSendTo(View view){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + "info@tarc.edu.my"));
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"No app to handle this",Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    public void showDial(View view){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + "034123456"));
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"No app to handle this",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
